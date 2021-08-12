ced42c29a2cc9670302b9ca228cd3d7962ca1faa
Alin-Gabriel Musat <musataling@gmail.com>
2021-08-12 18:49:27 +0300

    a

--- lib/main.dart
+++ lib/main.dart
@@ -41,67 +41,159 @@ class AnimatedBotomNavigationBar extends StatefulWidget {
 class _AnimatedBotomNavigationBarState extends State<AnimatedBotomNavigationBar>
     with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
-  late Animation _animation;
-  late Animation _animation2;
-  late Animation _animation3;
+  late Animation<double> _moveIconUpAnimation;
+  late Animation<double> _reduceContainerWidthAnimation;
+  late Animation<double> _containerIncreaseOpacityAnimation;
+  late Animation<double> _scaleDownIconAnimation;
+  late Animation<double> _translateIconBackToOriginAnimation;
+  late Animation<double> _scaleIconBackAnimation;
+  late Animation<double> _containerDecreaseOpacityAnimation;
+  late Animation<double> _restoreContainerWidthAnimation;
+  late Animation<double> _scaleUpContainerAnimation;
+  late Animation<double> _scaleDownContainerAnimation;
+  late Animation<double> _rowReduceMarginAnimation;
+  late Animation<double> _rowIncreaseMarginAnimation;
 
   int _selected = 0;
-  bool _reappearing = false;
-  bool _shrinking = false;
+
+  bool shrinking = false;
 
   @override
   void initState() {
     super.initState();
     _animationController = AnimationController(
         duration: Duration(milliseconds: 1000), vsync: this);
-    _animation = Tween<double>(begin: 0, end: 100).animate(
+    _moveIconUpAnimation = Tween<double>(begin: 0, end: 100).animate(
       CurvedAnimation(
           parent: _animationController,
           curve: Interval(
             0.0,
-            0.4,
+            0.330,
             curve: Curves.easeIn,
           )),
     );
 
-    _animation.addListener(() {
-      setState(() {});
-    });
-
-    _animation2 = Tween<double>(begin: 1, end: 0).animate(
+    _scaleDownIconAnimation = Tween<double>(begin: 1, end: 0.01).animate(
       CurvedAnimation(
           parent: _animationController,
           curve: Interval(
-            0.4,
-            0.6,
+            0.330,
+            0.500,
             curve: Curves.easeIn,
           )),
     );
 
-    _animation2.addListener(() {
-      setState(() {});
-    });
-
-    _animation3 = Tween<double>(begin: 0, end: 1).animate(
+    _translateIconBackToOriginAnimation =
+        Tween<double>(begin: 0, end: 100).animate(
       CurvedAnimation(
           parent: _animationController,
           curve: Interval(
-            0.6,
+            0.500,
+            0.510,
+            curve: Curves.easeIn,
+          )),
+    );
+
+    _scaleIconBackAnimation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+          parent: _animationController,
+          curve: Interval(
+            0.510,
+            0.660,
+            curve: Curves.easeIn,
+          )),
+    );
+
+    _reduceContainerWidthAnimation = Tween<double>(begin: 0, end: 32).animate(
+      CurvedAnimation(
+          parent: _animationController,
+          curve: Interval(
+            0.0,
+            0.375,
+            curve: Curves.easeIn,
+          )),
+    );
+
+    _restoreContainerWidthAnimation = Tween<double>(begin: 32, end: 0).animate(
+      CurvedAnimation(
+          parent: _animationController,
+          curve: Interval(
+            0.375,
+            0.5,
+            curve: Curves.easeIn,
+          )),
+    );
+
+    _containerIncreaseOpacityAnimation =
+        Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+          parent: _animationController,
+          curve: Interval(
+            0.0,
+            0.375,
+            curve: Curves.ease,
+          )),
+    );
+
+    _rowReduceMarginAnimation = Tween<double>(begin: 0, end: 24).animate(
+      CurvedAnimation(
+          parent: _animationController,
+          curve: Interval(
+            0.0,
+            0.375,
+            curve: Curves.ease,
+          )),
+    );
+
+    _containerDecreaseOpacityAnimation =
+        Tween<double>(begin: 1, end: 0).animate(
+      CurvedAnimation(
+          parent: _animationController,
+          curve: Interval(
+            0.8,
             1,
-            curve: Curves.easeIn,
+            curve: Curves.ease,
           )),
     );
 
-    _animation3.addListener(() {
+    _rowIncreaseMarginAnimation = Tween<double>(begin: 0, end: -24).animate(
+      CurvedAnimation(
+          parent: _animationController,
+          curve: Interval(
+            0.375,
+            1,
+            curve: Curves.ease,
+          )),
+    );
+
+    _scaleUpContainerAnimation = Tween<double>(begin: 1, end: 1.2).animate(
+      CurvedAnimation(
+          parent: _animationController,
+          curve: Interval(
+            0.5,
+            0.700,
+            curve: Curves.ease,
+          )),
+    );
+
+    _scaleDownContainerAnimation = Tween<double>(begin: 1.2, end: 1).animate(
+      CurvedAnimation(
+          parent: _animationController,
+          curve: Interval(
+            0.700,
+            0.9,
+            curve: Curves.ease,
+          )),
+    );
+
+    _moveIconUpAnimation.addListener(() {
       setState(() {});
     });
 
-    _animation3.addStatusListener((status) {
+    _moveIconUpAnimation.addStatusListener((status) {
       if (status == AnimationStatus.completed) {
-        setState(() {
-          _reappearing = false;
-          _animationController.reset();
-        });
+        _animationController.reset();
+        shrinking = false;
       }
     });
   }
@@ -112,110 +204,167 @@ class _AnimatedBotomNavigationBarState extends State<AnimatedBotomNavigationBar>
     setState(() {
       _selected = index;
       _animationController.forward();
-      _shrinking = true;
+      shrinking = true;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    double horizontalMargin = 16.0;
-    double iconWidth = 48.0;
-    double spacing =
-        ((MediaQuery.of(context).size.width - horizontalMargin * 2) -
-                iconWidth * 5) /
-            4;
-
-    print(_animation.value.toString() +
+    print(_scaleDownIconAnimation.value.toString() +
         " " +
-        _animation2.value.toString() +
-        " " +
-        _animation3.value.toString());
+        _scaleIconBackAnimation.value.toString());
 
-    if (_animation2.value == 0 && _reappearing == false) {
-      setState(() {
-        print("reappring");
-        _reappearing = true;
-      });
-    }
+    double horizMargin = 16.0;
+
+    double inBetweenItems =
+        ((MediaQuery.of(context).size.width - horizMargin * 2) - 48 * 5) / 4;
 
     var leftOffsets = [0.0, 0.0, 0.0, 0.0, 0.0];
     for (int i = 0; i < 5; i++) {
-      leftOffsets[i] = horizontalMargin + (iconWidth + spacing) * i;
+      leftOffsets[i] = horizMargin + (inBetweenItems + 48) * i;
     }
 
-    if (_shrinking) {
+    if (shrinking) {
+      // print("Cont anim color" + _containerAnimationColor.value.toString());
+      // print("Cont revers anim color" +
+      //     _reversecontainerAnimationColor.value.toString());
+
       for (int i = 0; i < _selected; i++) {
-        leftOffsets[i] += _animation.value / 5;
+        leftOffsets[i] += _rowReduceMarginAnimation.value;
       }
       for (int i = _selected + 1; i < 5; i++) {
-        leftOffsets[i] -= _animation.value / 5;
+        leftOffsets[i] -= _rowReduceMarginAnimation.value;
       }
+
+      if (_rowIncreaseMarginAnimation.value < 1) {
+        for (int i = 0; i < _selected; i++) {
+          leftOffsets[i] += _rowIncreaseMarginAnimation.value;
+        }
+        for (int i = _selected + 1; i < 5; i++) {
+          leftOffsets[i] -= _rowIncreaseMarginAnimation.value;
+        }
+      }
+
+      print(leftOffsets);
     }
 
-    var bottomOffsets = [0.0, 0.0, 0.0, 0.0, 0.0];
+    var icons = [
+      NavbarItem(
+        iconData: CupertinoIcons.house,
+        onPressed: () => onPageSelected(0),
+      ),
+      NavbarItem(
+          iconData: CupertinoIcons.bookmark,
+          onPressed: () => onPageSelected(1)),
+      NavbarItem(
+        iconData: CupertinoIcons.smallcircle_circle,
+        onPressed: () => onPageSelected(2),
+      ),
+      NavbarItem(
+        iconData: CupertinoIcons.heart,
+        onPressed: () => onPageSelected(3),
+      ),
+      NavbarItem(
+        iconData: CupertinoIcons.person,
+        onPressed: () => onPageSelected(4),
+      ),
+    ];
+
+    var iconsFilled = [
+      NavbarItem(
+        iconData: CupertinoIcons.house_fill,
+        onPressed: () => onPageSelected(0),
+      ),
+      NavbarItem(
+          iconData: CupertinoIcons.bookmark_fill,
+          onPressed: () => onPageSelected(1)),
+      NavbarItem(
+        iconData: CupertinoIcons.smallcircle_circle_fill,
+        onPressed: () => onPageSelected(2),
+      ),
+      NavbarItem(
+        iconData: CupertinoIcons.heart_fill,
+        onPressed: () => onPageSelected(3),
+      ),
+      NavbarItem(
+        iconData: CupertinoIcons.person_fill,
+        onPressed: () => onPageSelected(4),
+      ),
+    ];
+
+    var items = [];
     for (int i = 0; i < 5; i++) {
-      bottomOffsets[i] = _selected == i
-          ? _reappearing
-              ? 0
-              : _animation.value
-          : 0;
+      items.add(Positioned(
+        left: leftOffsets[i],
+        bottom: 32 + (_selected == i ? _moveIconUpAnimation.value : 0),
+        child: Transform.translate(
+          offset: Offset(0,
+              _selected == i ? _translateIconBackToOriginAnimation.value : 0),
+          child: Transform.scale(
+              scale: _selected == i
+                  ? _scaleDownIconAnimation.value > 0.01
+                      ? _scaleDownIconAnimation.value
+                      : _scaleIconBackAnimation.value
+                  : 1,
+              child: _selected == i ? iconsFilled[i] : icons[i]),
+        ),
+      ));
     }
 
-    var scales = [0.0, 0.0, 0.0, 0.0, 0.0];
-    for (int i = 0; i < 5; i++) {
-      scales[i] = _selected == i
-          ? _reappearing
-              ? _animation3.value
-              : _animation2.value
-          : 1;
+    var containerHorizPadding = [
+      _reduceContainerWidthAnimation.value,
+      _reduceContainerWidthAnimation.value
+    ];
+    if (_selected == 0) {
+      containerHorizPadding[0] = 0;
     }
 
-    if (_shrinking) {
-      horizontalMargin += _animation.value / 5;
+    if (_selected == 4) {
+      containerHorizPadding[1] = 0;
+    }
+
+    if (_reduceContainerWidthAnimation.value == 32.0) {
+      containerHorizPadding = [
+        _restoreContainerWidthAnimation.value,
+        _restoreContainerWidthAnimation.value
+      ];
+      if (_selected == 0) {
+        containerHorizPadding[0] = 0;
+      }
+
+      if (_selected == 4) {
+        containerHorizPadding[1] = 0;
+      }
     }
 
     return Container(
       height: 200,
       width: double.infinity,
-      color: Colors.transparent,
       child: Stack(
-        alignment: Alignment.centerLeft,
+        alignment: Alignment.bottomCenter,
         children: [
-          NavbarItem(
-            iconData: CupertinoIcons.home,
-            left: leftOffsets[0],
-            bottomOffset: bottomOffsets[0],
-            scale: scales[0],
-            onPressed: () => onPageSelected(0),
+          Transform.scale(
+            scale: _scaleUpContainerAnimation.value < 1.2
+                ? _scaleUpContainerAnimation.value
+                : _scaleDownContainerAnimation.value,
+            child: Opacity(
+              opacity: _containerIncreaseOpacityAnimation.value < 1
+                  ? _containerIncreaseOpacityAnimation.value
+                  : _containerDecreaseOpacityAnimation.value,
+              child: Container(
+                height: 98,
+                margin: EdgeInsets.only(
+                    left: containerHorizPadding[0],
+                    right: containerHorizPadding[1]),
+                decoration: BoxDecoration(
+                    color: Color(0xffA7B2E4),
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(24),
+                        topRight: Radius.circular(24))),
+              ),
+            ),
           ),
-          NavbarItem(
-            iconData: CupertinoIcons.bookmark,
-            left: leftOffsets[1],
-            bottomOffset: bottomOffsets[1],
-            scale: scales[1],
-            onPressed: () => onPageSelected(1),
-          ),
-          NavbarItem(
-            iconData: CupertinoIcons.smallcircle_circle,
-            left: leftOffsets[2],
-            bottomOffset: bottomOffsets[2],
-            scale: scales[2],
-            onPressed: () => onPageSelected(2),
-          ),
-          NavbarItem(
-            iconData: CupertinoIcons.heart,
-            left: leftOffsets[3],
-            bottomOffset: bottomOffsets[3],
-            scale: scales[3],
-            onPressed: () => onPageSelected(3),
-          ),
-          NavbarItem(
-            iconData: CupertinoIcons.person,
-            left: leftOffsets[4],
-            bottomOffset: bottomOffsets[4],
-            scale: scales[4],
-            onPressed: () => onPageSelected(4),
-          ),
+          ...items,
         ],
       ),
     );
@@ -227,37 +376,20 @@ class NavbarItem extends StatelessWidget {
 
   final Function onPressed;
 
-  final double bottomOffset;
-
-  final double scale;
-
-  final double left;
-
-  const NavbarItem(
-      {Key? key,
-      required this.iconData,
-      required this.onPressed,
-      required this.left,
-      this.scale = 1,
-      this.bottomOffset = 0})
-      : super(key: key);
+  const NavbarItem({
+    Key? key,
+    required this.iconData,
+    required this.onPressed,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      left: left,
-      bottom: 32 + bottomOffset,
-      child: Transform.scale(
-        scale: scale,
-        child: Container(
-          width: 48,
-          height: 48,
-          child: IconButton(
-            onPressed: () => onPressed(),
-            icon: Icon(iconData),
-          ),
-        ),
-      ),
-    );
+    return Container(
+        width: 48,
+        height: 48,
+        child: IconButton(
+          onPressed: () => onPressed(),
+          icon: Icon(iconData),
+        ));
   }
 }
